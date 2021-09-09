dcc36712cd5463d55dee1c8762f92475d40d68ba
Viktor Forizs <forizs.viktor1987@gmail.com>
2021-09-10 01:59:44 +0200

    frontend and backend heroku

--- server/server_app/views.py
+++ server/server_app/views.py
@@ -1,1 +1,17 @@
 from django.shortcuts import render
+from .models import SecretModel
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+import os
+
+
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
--- server/urls.py
+++ server/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic.base import TemplateView
 from graphene_django.views import GraphQLView
 from .schema import schema
 
 
 urlpatterns = [
-    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema))
+    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
